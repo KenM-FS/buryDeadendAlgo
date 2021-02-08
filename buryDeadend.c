06212a2ac3017975ee1533540be227ef18d62843
FlyingSeaweed <b1018083@fun.ac.jp>
2021-02-08 17:06:56 +0900

    Add comments for previous codes and add function

--- buryDeadend.c
+++ buryDeadend.c
@@ -5,34 +5,44 @@
 #ifndef WORLD_SIZE
 #define WORLD_SIZE 8
 #endif
-
+/* 捕食者の数値表現 */
 #ifndef PREDATOR
 #define PREDATOR 1
 #endif
-
+/* 獲物の数値表現 */
 #ifndef PREY
 #define PREY 10
 #endif
-
+/* 障害物の数値表現 */
 #ifndef OBSTACLE
 #define OBSTACLE -1
 #endif
-
+/* 自由格子の数値表現 */
 #ifndef FREE
 #define FREE 0
 #endif
 
+/* オリジナルのマップ */
 #ifndef ORIGINAL
 #define ORIGINAL 0
 #endif
-
+/* 加工したマップ */
 #ifndef BRUIED
 #define BRUIED 1
 #endif
 
+/*======ここから本題============================================================*/
+/*
+ * 参照したい配列の添字が有効か判断する
+ */
+int index(int i, int j){
+  if(i<0 || i>WORLD_SIZE || j<0 || j>WORLD_SIZE) return WORLD_SIZE*WORLD_SIZE;
+  return WORLD_SIZE*i + j;
+}
+
+/*======ここから本題とは関係ない部分===============================================*/
 /* 
  * 格子世界を読み込む関数 
- * 講義で配布されたものを使用
  */
 int readBattlefield(int battlefield[WORLD_SIZE][WORLD_SIZE], char *filename){
   FILE *fp;
@@ -53,7 +63,9 @@ int readBattlefield(int battlefield[WORLD_SIZE][WORLD_SIZE], char *filename){
     return(0);
   }
 }
-
+/*
+ * 格子世界を出力する関数
+ */
 int printWorld(int battlefield[WORLD_SIZE][WORLD_SIZE], int message){
   if(message == ORIGINAL){
     printf("Original Map\n");
@@ -91,6 +103,7 @@ int printWorld(int battlefield[WORLD_SIZE][WORLD_SIZE], int message){
 }
 
 int main(int argc, char *argv[]){
+  // コマンドライン引数で読み込むファイルを変更できるように
   int battlefield[WORLD_SIZE][WORLD_SIZE];
   char filename[256];
   strcpy(filename, "battlefield.dat");
@@ -98,11 +111,13 @@ int main(int argc, char *argv[]){
     strncpy(filename, argv[1], 256);
   }
 
+  // ファイルから格子世界の読み込み
   if(readBattlefield(battlefield, filename) == 1){
     fprintf(stderr, "File read failed\n");
     return(1);
   }
 
+  // 初期状態の格子世界を出力
   printWorld(battlefield, ORIGINAL);
 
   return(0);
